dffeeca5dc322e1b2745fe6cf4b8d8e4931a3cdc
olegsvs <oleg.texet@gmail.com>
2016-12-01 19:47:53 +0000

    including patches

---
+++ patches/apply-patches.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd ../../../..
+cd frameworks/av
+git apply -v ../../device/archos/persimmon/patches/frameworks_av/0001-Disable-usage-of-get_capture_position.patch
+cd ../..
+cd system/core
+git apply -v ../../device/archos/persimmon/patches/system_core/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
+git apply -v ../../device/archos/persimmon/patches/system_core/0002-Changes-for-more-level-log.patch
+cd ../..
+cd system/netd
+git apply -v ../../device/archos/persimmon/patches/system_netd/0001-Enable-Tethering.patch
+cd ../..
+cd system/sepolicy
+git apply -v ../../device/archos/persimmon/patches/system_sepolicy/0001-Revert-back-to-version-29.patch
+cd ../..
+echo Patches Applied Successfully!
