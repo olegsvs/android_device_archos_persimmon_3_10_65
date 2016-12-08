666d81350c80a3982436ca6064df5d863c360844
olegsvs <oleg.texet@gmail.com>
2016-12-08 11:04:20 +0000

    cln logger

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -4,8 +4,8 @@ cd frameworks/av
 git apply -v ../../device/archos/persimmon/patches/frameworks_av/0001-Disable-usage-of-get_capture_position.patch
 cd ../..
 cd system/core
-git apply -v ../../device/archos/persimmon/patches/system_core/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
-git apply -v ../../device/archos/persimmon/patches/system_core/0002-Changes-for-more-level-log.patch
+git apply -v ../../device/archos/persimmon/patches/system_core/0001-remove-CAP_SYS_NICE-from-surfaceflinger-and-cln-logg.patch
+#git apply -v ../../device/archos/persimmon/patches/system_core/0002-Changes-for-more-level-log.patch
 cd ../..
 cd system/netd
 git apply -v ../../device/archos/persimmon/patches/system_netd/0001-Enable-Tethering.patch
