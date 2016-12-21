ce0b023d88c77edbd0bd37cf73f01cb58864f8cc
Oleg Sinelnikov <oleg.texet@gmail.com>
2016-12-21 11:51:27 +0300

    changes for last cm updates - fix kernel panic on start

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -5,7 +5,7 @@ git apply -v ../../device/archos/persimmon/patches/frameworks_av/0001-Disable-us
 cd ../..
 cd system/core
 git apply -v ../../device/archos/persimmon/patches/system_core/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
-git apply -v ../../device/archos/persimmon/patches/system_core/0002-Changes-for-more-level-log.patch
+#git apply -v ../../device/archos/persimmon/patches/system_core/0002-Changes-for-more-level-log.patch
 cd ../..
 cd system/netd
 git apply -v ../../device/archos/persimmon/patches/system_netd/0001-Revert-Don-t-start-tethering-if-IPv6-RPF-is-not-supp.patch
